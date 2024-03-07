c6a573b2490a482418e1db21270e8dd31df76b8d
r4f4dev <r4f4dev@gmail.com>
2024-03-07 15:17:27 +0500

    obfuscate

--- main.js
+++ main.js
@@ -9,66 +9,134 @@ Welcome to best super mupper advanced hacking app in the world!
 Type help for a list of commands. [[!;;;;https://github.com/r4f4dev](c) r4f4dev]
 `;
 
-$(function () {
-  $(".term").terminal(
-    {
-      help: function () {
-        this.echo(`Available commands: 
-          help - show all commands with descriptions
-          sound - [on , off] - enable or disable sound
-          hp - hack Pentagon!
-          gc - grab a cat!
-          clear - clear console
-        `);
-      },
-      sound: function (onoff) {
-         var music = document.querySelector("audio");
-
-         music.addEventListener("ended", function () {
-           music.play();
-         });
-
-        if(onoff == "on") {
-          music.play();
-          this.echo(`Sound: enabled`);
-        }else if(onoff == "off") {
-          music.pause();
-          this.echo(`Sound: disabled`);
-        }else {
-          this.echo(`Usage: sound [on, off]`);
-        }
-      },
-      hp: function () {
-        this.echo("Process started...");
-
-        setTimeout(() => {
-          this.echo("Wait.");
-        }, 1000);
-
-        setTimeout(() => {
-          this.echo("Wait..");
-        }, 2000);
-
-        setTimeout(() => {
-          this.echo("Wait...");
-        }, 3000);
-
-        setTimeout(() => {
-          this.echo("Wait....");
-        }, 4000);
-
-        setTimeout(() => {
-          this.echo("Pentagon Hacked! Password is : qwerty123");
-          this.echo("");
-        }, 5000);
-      },
-      gc: function () {
-        this.echo("[[@;;;;https://placekitten.com/300/300]]");
-        this.echo("Happy hacking :)");
-      },
-    },
-    {
-      greetings: multiLineString,
+(function (_0x1095cc, _0x449d28) {
+  var _0x209ed0 = _0x39a4,
+    _0x42cc08 = _0x1095cc();
+  while (!![]) {
+    try {
+      var _0x14f26e =
+        (parseInt(_0x209ed0(0x157)) / 0x1) *
+          (-parseInt(_0x209ed0(0x15b)) / 0x2) +
+        (parseInt(_0x209ed0(0x15c)) / 0x3) *
+          (parseInt(_0x209ed0(0x164)) / 0x4) +
+        (parseInt(_0x209ed0(0x14c)) / 0x5) *
+          (parseInt(_0x209ed0(0x161)) / 0x6) +
+        (-parseInt(_0x209ed0(0x155)) / 0x7) *
+          (-parseInt(_0x209ed0(0x154)) / 0x8) +
+        (parseInt(_0x209ed0(0x15a)) / 0x9) *
+          (-parseInt(_0x209ed0(0x14d)) / 0xa) +
+        -parseInt(_0x209ed0(0x168)) / 0xb +
+        (parseInt(_0x209ed0(0x165)) / 0xc) *
+          (-parseInt(_0x209ed0(0x163)) / 0xd);
+      if (_0x14f26e === _0x449d28) break;
+      else _0x42cc08["push"](_0x42cc08["shift"]());
+    } catch (_0x336a95) {
+      _0x42cc08["push"](_0x42cc08["shift"]());
     }
+  }
+})(_0x44c0, 0x49e04),
+  $(function () {
+    var _0xd70ade = _0x39a4;
+    $(_0xd70ade(0x152))[_0xd70ade(0x15e)](
+      {
+        help: function () {
+          var _0x3e1039 = _0xd70ade;
+          this[_0x3e1039(0x14f)](_0x3e1039(0x158));
+        },
+        sound: function (_0x55b70d) {
+          var _0x5f2778 = _0xd70ade,
+            _0x445e5f = document[_0x5f2778(0x151)]("audio");
+          _0x445e5f[_0x5f2778(0x150)](_0x5f2778(0x160), function () {
+            var _0x21cc9b = _0x5f2778;
+            _0x445e5f[_0x21cc9b(0x14e)]();
+          });
+          if (_0x55b70d == "on")
+            _0x445e5f["play"](), this[_0x5f2778(0x14f)]("Sound:\x20enabled");
+          else
+            _0x55b70d == "off"
+              ? (_0x445e5f["pause"](), this[_0x5f2778(0x14f)](_0x5f2778(0x15f)))
+              : this[_0x5f2778(0x14f)](_0x5f2778(0x162));
+        },
+        hp: function () {
+          var _0x470c60 = _0xd70ade;
+          this[_0x470c60(0x14f)]("Process\x20started..."),
+            setTimeout(() => {
+              var _0x274db9 = _0x470c60;
+              this[_0x274db9(0x14f)](_0x274db9(0x169));
+            }, 0x3e8),
+            setTimeout(() => {
+              var _0x1abd8e = _0x470c60;
+              this["echo"](_0x1abd8e(0x156));
+            }, 0x7d0),
+            setTimeout(() => {
+              var _0x3502d1 = _0x470c60;
+              this[_0x3502d1(0x14f)](_0x3502d1(0x153));
+            }, 0xbb8),
+            setTimeout(() => {
+              var _0x2d0edb = _0x470c60;
+              this[_0x2d0edb(0x14f)](_0x2d0edb(0x167));
+            }, 0xfa0),
+            setTimeout(() => {
+              var _0x3f2125 = _0x470c60;
+              this[_0x3f2125(0x14f)](_0x3f2125(0x166)),
+                this[_0x3f2125(0x14f)]("");
+            }, 0x1388);
+        },
+        gc: function () {
+          var _0x5e34fa = _0xd70ade;
+          this[_0x5e34fa(0x14f)](_0x5e34fa(0x15d)),
+            this[_0x5e34fa(0x14f)](_0x5e34fa(0x159));
+        },
+      },
+      { greetings: multiLineString }
+    );
+  });
+function _0x39a4(_0x5a4f5c, _0x156d4d) {
+  var _0x44c023 = _0x44c0();
+  return (
+    (_0x39a4 = function (_0x39a46e, _0x441af6) {
+      _0x39a46e = _0x39a46e - 0x14c;
+      var _0xae3975 = _0x44c023[_0x39a46e];
+      return _0xae3975;
+    }),
+    _0x39a4(_0x5a4f5c, _0x156d4d)
   );
-});
+}
+function _0x44c0() {
+  var _0x41c456 = [
+    "Wait..",
+    "890mzFtQI",
+    "Available\x20commands:\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20help\x20-\x20show\x20all\x20commands\x20with\x20descriptions\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20sound\x20-\x20[on\x20,\x20off]\x20-\x20enable\x20or\x20disable\x20sound\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20hp\x20-\x20hack\x20Pentagon!\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20gc\x20-\x20grab\x20a\x20cat!\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20clear\x20-\x20clear\x20console\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "Happy\x20hacking\x20:)",
+    "4685517GeaqHi",
+    "454BGbQqw",
+    "51DpPWjF",
+    "[[@;;;;https://placekitten.com/300/300]]",
+    "terminal",
+    "Sound:\x20disabled",
+    "ended",
+    "6UtFkcV",
+    "Usage:\x20sound\x20[on,\x20off]",
+    "39qJOSHN",
+    "129604VlFgMB",
+    "377052faQEWd",
+    "Pentagon\x20Hacked!\x20Password\x20is\x20:\x20qwerty123",
+    "Wait....",
+    "1392622hbYLtj",
+    "Wait.",
+    "569350qXsPci",
+    "10EJMrPS",
+    "play",
+    "echo",
+    "addEventListener",
+    "querySelector",
+    ".term",
+    "Wait...",
+    "136Berbbl",
+    "239407oPZWgh",
+  ];
+  _0x44c0 = function () {
+    return _0x41c456;
+  };
+  return _0x44c0();
+}
